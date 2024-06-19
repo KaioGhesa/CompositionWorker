06ed28e3fd83695e1e0f3ac31f53179838f6d4bb
Ghesa <124006021+KaioGhesa@users.noreply.github.com>
2024-06-19 16:26:59 -0300

    Add files via upload

---
+++ CompositionWorker/CompositionWorker/Entites/HourContract.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositionWorker.Entites
+{
+    internal class HourContract
+    {
+        public DateTime date { get; set; }
+        public double ValuePerHour { get; set; }
+        public int Hours { get; set; }
+
+
+        public HourContract()
+        {
+
+        }
+
+        public HourContract(DateTime date, double valuePerHour, int hours)
+        {
+            this.date = date;
+            this.ValuePerHour = valuePerHour;
+            this.Hours = hours;
+        }
+
+        public double TotalValue()
+        {
+            return Hours * ValuePerHour;
+        }
+    }
+}
---
+++ CompositionWorker/CompositionWorker/Entites/Worker.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using CompositionWorker.Entites.Enums;
+
+namespace CompositionWorker.Entites
+{
+    internal class Worker
+    {
+
+        public string name { get; set; }
+        public WorkerLevel level { get; set; }
+        public double BaseSalary { get; set; }
+        public Departament Departament { get; set; }
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();
+
+
+        public Worker()
+        {
+
+        }
+
+        public Worker(string name, WorkerLevel level, double baseSalary, Departament departament)
+        {
+            this.name = name;
+            this.level = level;
+            BaseSalary = baseSalary;
+            Departament = departament;
+        }
+
+        public void addContract(HourContract contract)
+        {
+            Contracts.Add(contract);
+        }
+
+        public void removeContract(HourContract contract)
+        {
+            Contracts.Remove(contract);
+        }
+
+        public double Income(int year, int month)
+        {
+            double sum = BaseSalary;
+            foreach (HourContract contract in Contracts)
+            {
+                if (contract.date.Year == year && contract.date.Month == month)
+                {
+                    sum += contract.TotalValue();
+                }
+            }
+
+            return sum;
+        }
+
+    }
+}
